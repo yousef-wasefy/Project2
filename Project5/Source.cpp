bfc4ff12ad7f5d6274027a5485df743647ab5a2a
Y.Wasefy <yousefalwasefy@gmail.com>
2024-12-10 23:56:36 +0200

    Add project files.

---
+++ Project5/Source.cpp
@@ -0,0 +1,34 @@
+//Yousuf Ahmed
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int number; 
+	int x ;
+	int z;
+	char answer;
+	do
+	{
+		x = 1;
+		cout << "Enter a number : ";
+		cin >> number;
+		z = number;
+		for (int n = 1;n <= 20;n++)
+		{
+			if (n == 5)
+				break;
+			for (int i = 1;i <= 5;i++)
+			{
+				number = z;
+				number *= x;
+				x++;
+				cout << number << "\t";
+			}
+			cout << "\n" << endl;
+		}
+		cout << "Do another (y/n) : ";
+		cin >> answer;
+	} while (answer == 'y');
+	return 0;
+}
